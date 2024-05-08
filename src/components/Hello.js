597e172677395e7dd2f9d82240a82c7aa42c6a3f
Emmanuel Tar <emmanueltar14@gmail.com>
2024-05-08 13:15:16 +0100

    JSX Update on React

---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const Hello=()=>{
+    /*<div>
+        <h1>
+            Hello Emmanuel
+        </h1>
+    </div>
+    */
+
+    return React.createElement('div',null,
+    React.createElement('h1',null,'Hello Brother')
+)
+}
+
+export default Hello
