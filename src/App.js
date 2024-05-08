8c402fae0231159ce9c011646ee9a67ff9e12ce3
Emmanuel Tar <emmanueltar14@gmail.com>
2024-05-08 11:55:27 +0100

    update on React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet.js'
-
+import Welcome from './components/Welcome.js'
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-function Greet (){
-    return <h1>What's Up Brother</h1>
-}
+//function Greet (){
+    //return <h1>What's Up Brother</h1>
+//}
+const Greet = () => <h1>What's Up Brother</h1>
 
-export default Greet
+export default Greet 
