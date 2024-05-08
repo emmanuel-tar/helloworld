bd21586e09de464e80bc47a8d8368a300ac85685
Emmanuel Tar <emmanueltar14@gmail.com>
2024-05-08 15:06:50 +0100

    Update .. Using Props vs State Function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Greet from './components/Greet.js'
 import Welcome from './components/Welcome.js'
 import Hello from './components/Hello.js'
+import Message from './components/Message.js';
 
 function App() {
   return (
     <div className="App">
+    <Message />
       {/*<Greet /> */}
       {/*<Welcome /> */}
       <Hello />
